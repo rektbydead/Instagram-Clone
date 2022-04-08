8f45fce70279b69d19aa1272cfbca6d764d2cf0c
Ruben Lousada <rlousada123456@gmail.com>
2022-04-08 16:34:00 +0100

    Create models

--- InstagramClone/main/models.py
+++ InstagramClone/main/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Publication:
+class Publication():
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
-    date = models.DateTimeField(auto_now=False)
+    date = models.DateTimeField(auto_now=False, auto_now_add=True, blank=False)
+
+class Story(Publication):
+    pass
+
+
