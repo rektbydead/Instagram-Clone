c03ebe0ad93b58b849626fca1a36819b976a732c
Ruben Lousada <rlousada123456@gmail.com>
2022-04-08 22:13:22 +0100

    Do base.html

--- InstagramClone/register/models.py
+++ InstagramClone/register/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Account(User):
-    description = models.CharField(max_length=300)
+    description = models.CharField(max_length=120, null=False, blank=True)
+    name = models.CharField(max_length=30, null=False, blank=True)
+    photo = models.ImageField(null=False, blank=True)
 
 class Follower():
     user_being_followed = models.ForeignKey(Account, on_delete=models.CASCADE, null=False, blank=False)
