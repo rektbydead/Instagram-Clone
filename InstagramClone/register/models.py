9a89a4288257fa222c0a70dbba12b6f77d0e8079
Ruben Lousada <rlousada123456@gmail.com>
2022-04-08 19:53:18 +0100

    Modify models

--- InstagramClone/register/models.py
+++ InstagramClone/register/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Account(User):
+    description = models.CharField(max_length=300)
+
+class Follower():
+    user_being_followed = models.ForeignKey(Account, on_delete=models.CASCADE, null=False, blank=False)
+    follower = models.ForeignKey(Account, on_delete=models.CASCADE, null=False, blank=False)
