d14ad3b389c1e0b8a422bdda770da4ddd5ddcceb
Ruben Lousada <rlousada123456@gmail.com>
2022-04-10 13:50:49 +0100

    Done base.html & Start Register.html & Start Url Patther and views

--- InstagramClone/main/models.py
+++ InstagramClone/main/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Publication():
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
     date = models.DateTimeField(auto_now=False, auto_now_add=True, blank=False)
+    description = models.TextField(max_length=300, blank=True, null=False)
 
 class Story(Publication):
     pass
--- InstagramClone/main/views.py
+++ InstagramClone/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def default(response):
+    pass
+
+def inbox(response):
+    pass
+
+def profile(response, profile_id):
+    pass
