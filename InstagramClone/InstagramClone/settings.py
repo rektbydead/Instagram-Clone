84d2550dff6b90ec3da20bdbf0c9dea5cd76db4f
Ruben Lousada <rlousada123456@gmail.com>
2022-04-10 18:26:13 +0100

    Add redirect on login and logout

--- InstagramClone/InstagramClone/settings.py
+++ InstagramClone/InstagramClone/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
+    'register.apps.RegisterConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL="/"
+LOGOUT_REDIRECT_URL="/"
