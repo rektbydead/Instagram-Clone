ad3a51381c01e4047813b2e39002ffc8a60ad9ba
Ruben Lousada <rlousada123456@gmail.com>
2022-04-10 18:56:46 +0100

    Update register.html & others

--- InstagramClone/register/forms.py
+++ InstagramClone/register/forms.py
@@ -3,8 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    name = forms.CharField(max_length=16)
+    email = forms.EmailField(blank=False, null=False)
+    name = forms.CharField(max_length=16, blank=False, null=False)
 
     class Meta:
         model = User
--- InstagramClone/register/views.py
+++ InstagramClone/register/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import RegisterForm
 
 # Create your views here.
+def register(response):
+    if response.user.is_authenticated == True:
+        return render() # TODO: Render main instagram page
+
+    if response.method == 'POST':
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+        
+        return render() # TODO: Render main instagram page
+    else:
+        form = RegisterForm()
+    
+    return render(response, 'register/register.html', {'form': form})
