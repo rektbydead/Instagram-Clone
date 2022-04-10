9204ff02c8518e2b70cda80405f4f588ad1de9be
Ruben Lousada <rlousada123456@gmail.com>
2022-04-10 18:29:38 +0100

    Migration complete

--- InstagramClone/main/urls.py
+++ InstagramClone/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-url_pattern = {
+urlpatterns = {
     path("", views.default, name="home"),
     path("direct/inbox/", views.inbox, "inbox_messages"),
     path("<str:profile_id>", views.profile, "profile")
