3681fb892261780a303273691912b74105fc2c48
Ruben Lousada <rlousada123456@gmail.com>
2022-04-11 16:37:04 +0100

    Add field to form on register & Create login function

--- InstagramClone/register/views.py
+++ InstagramClone/register/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import RegisterForm
-
+import json
 # Create your views here.
 def register(response):
     if response.user.is_authenticated == True:
@@ -12,8 +12,15 @@ def register(response):
             form.save()
         
             pass # TODO: Render main instagram page
+        else:
+            form.first_error = list(form.errors.items())[0][1]
 
+            print(form.first_error)
     else:
         form = RegisterForm()
-    
+
+
     return render(response, 'register/register.html', {"form": form})
+
+def login(response):
+    pass
