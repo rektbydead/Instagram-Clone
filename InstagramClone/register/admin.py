846be02e80ded4432f8c38b6c80b57eb527ec31b
Ruben Lousada <rlousada123456@gmail.com>
2022-04-11 16:35:44 +0100

    Register Account model

--- InstagramClone/register/admin.py
+++ InstagramClone/register/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Account
+from django.utils.translation import gettext, gettext_lazy as _
 
 # Register your models here.
+
 admin.site.register(Account)
+#admin.site.register(Follower)
