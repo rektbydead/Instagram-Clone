7a6586b43ba7b6b52f123dbfcada2f7cb97fd344
Ruben Lousada <rlousada123456@gmail.com>
2022-04-14 16:31:01 +0100

    Add photo to publication

--- InstagramClone/main/models.py
+++ InstagramClone/main/models.py
@@ -6,6 +6,8 @@ class Publication(models.Model):
     user = models.ForeignKey(Account, on_delete=models.CASCADE, null=False, blank=False)
     date = models.DateTimeField(auto_now=False, auto_now_add=True, blank=False)
     description = models.TextField(max_length=300, blank=True, null=False)
+    photo = models.ImageField(null=False, blank=False)
+
 
 class Story(Publication):
     pass
