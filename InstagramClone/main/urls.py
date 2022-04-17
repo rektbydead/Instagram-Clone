5cc92b518917815f484f0923e4b6aced9f8987e7
Ruben Lousada <rlousada123456@gmail.com>
2022-04-17 21:38:08 +0100

    Add user_exists page & profile redirection done (view)

--- InstagramClone/main/urls.py
+++ InstagramClone/main/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = {
     path("", views.default, name="home"),
+    path("<str:username>", views.profile, name="profile"),
     path("direct/inbox/", views.inbox, name="inbox_messages"),
-    path("<str:profile_id>", views.profile, name="profile")
     #path("explore/", views.inbox, "explore"),
     #path("accounts/edit/", views.edit, "edit"),
 }
--- InstagramClone/main/views.py
+++ InstagramClone/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from register.models import Account
 
 # Create your views here.
 def default(response):
@@ -7,5 +8,10 @@ def default(response):
 def inbox(response):
     pass
 
-def profile(response, profile_id):
-    pass
+def profile(response, username):
+    filter = Account.objects.filter(username=username)
+
+    if not filter:
+        return render(response, 'main/user_does_not_exists.html')
+
+    return render(response, 'main/user_exists.html', { "user": filter[0]})
